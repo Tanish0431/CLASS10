455eae04091656f06c8b7785a39c0515d683279c
Tanish0431 <94530165+Tanish0431@users.noreply.github.com>
2021-11-17 15:59:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var a = prompt("Enter the first number: ");
+var b = prompt("Enter the second number: ");
+//run by the comp once
+
+function setup() {
+    var mybutton = createButton("Click Me to Swap");
+    mybutton.mousePressed(swap);
+  }
+  
+  function swap() {
+    console.log(" I am swapping the numbers");
+    // swap
+    [b, a] = [a, b];
+    console.log(" Second number now is " + b);
+    console.log(" First number now is "  + a)
+  }
+  
+
+  
